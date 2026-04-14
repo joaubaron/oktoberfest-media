87770eafc8dd5e5f2886efccb24844f8fe6ddfc3
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-04-14 09:06:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -279,6 +279,10 @@ adicionarSwipes();
 window.addEventListener('resize', updateVideoPositionAndSize);
 }
 
+const cartazButton = getElementSafe("cartazButton");
+if (cartazButton) cartazButton.addEventListener("click", mostrarCartazAno);
+}
+
 // ======== FUNÇÕES AUXILIARES E DE MÍDIA ========
 function preloadMedia() {
 // Pré-carrega fotos do GitHub
